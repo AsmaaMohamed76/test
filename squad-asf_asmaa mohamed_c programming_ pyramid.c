0c341fd3ced00d6e78324caf6cd24920f5730ed1
AsmaaMohamed76 <113949635+AsmaaMohamed76@users.noreply.github.com>
2022-09-21 04:31:14 +0200

    Add files via upload

---
+++ squad-asf_asmaa mohamed_c programming_ pyramid.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int pyramid(int rows)
+{
+     int i, j;
+    char shape[rows+1];
+
+    
+    printf("Enter Number of Rows from 2 to 5 : ");
+    scanf("%d", &rows);
+      if( (rows<=5) & (rows>=2) )
+    for(i=1; i<=rows; i++)
+    { 
+        for(j=i; j<rows; j++)
+        {
+            printf(" ");
+            shape[rows+1+1]=' ';
+        }
+
+        for(j=1; j<=(2*i-1); j++)
+        {
+            printf("*");
+            shape[rows+1+1]='*';
+        }
+        printf("\n");
+    }
+          
+      else if(rows>=0){
+        printf("Please Enter Number From 2 to 5 : ");
+        return pyramid(rows);
+      }
+}
+
+int main()
+{
+      int rows;
+      pyramid( rows);
+    return 0;
+}
