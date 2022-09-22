05d9273255a1ce7af86551f7d9b59fe561ec359a
AsmaaMohamed76 <113949635+AsmaaMohamed76@users.noreply.github.com>
2022-09-22 14:01:26 +0200

    Add files via upload

---
+++ squad-asf_asmaa mohamed_pythonprogramming_basic _calculator.py
@@ -0,0 +1,6 @@
+num1 = input("Enter Your First Number : ")
+num2 = input("Enter Your Second Number : ")
+sum = float(num1) + float(num2)
+sub = float(num1) - float(num2)
+print("The summation is : " ,sum)
+print("The subtraction is : " ,sub)
---
+++ squad-asf_asmaa mohamed_pythonprogramming_sum_ave.py
@@ -0,0 +1,9 @@
+from math import *
+num1 = input("Enter Your First Number : ")
+num2 = input("Enter Your Second Number : ")
+num3 = input("Enter Your Second Number : ")
+
+sum = float(num1) + float(num2) + float(num3)
+avg = sum /3
+print("The summation is : " ,sum)
+print("The subtraction is : " ,avg)
